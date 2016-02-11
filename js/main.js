6f01e6250e70d65fc695bc81acb51c2ab481a34d
hanbyul-here <hanbyul@nyu.edu>
2016-02-11 15:49:35 -0500

    not messing up lat/lng

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/// Part from g0v/twgeojson
-/// Graphic Engine and Geo Data Init Functions
-
 var addGeoObject = function( bg, svgObject ) {
 
   var i,j, len, len1;
@@ -36,10 +33,14 @@ var addGeoObject = function( bg, svgObject ) {
       buildingGroup.add(mesh);
     }
   }
+  enableDownloadLink();
+}
 
+function enableDownloadLink() {
   var buildingObj = exportToObj()
 
   var exportA = document.getElementById('exportA');
+  exportA.className = "";
   exportA.download = 'tile.obj';
 
   var blob = new Blob([buildingObj], {type: 'text'});
@@ -49,11 +50,12 @@ var addGeoObject = function( bg, svgObject ) {
   document.getElementById('loading-bar').style.display = "none";
 }
 
+
 function init3d() {
 
   /// Global : renderer
   renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setClearColor( 0xb0b0b0 );
+  renderer.setClearColor( "0xb0b0b0" );
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   /// Global : scene
@@ -71,60 +73,27 @@ function init3d() {
 
 
   /// direct light
-  var light = new THREE.DirectionalLight( 0xcecece );
+  var light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 0.55, 0.8, 1.0 ).normalize();
   scene.add( light );
 
   /// ambient light
   var ambientLight = new THREE.AmbientLight(0x404040);
   scene.add( ambientLight );
-
-  // SKYDOME
-
-  var hemiLight = new THREE.HemisphereLight( 0xcecece, 0xcecece, 1.1 );
-  hemiLight.color.setHSL( 0.65, 0.7, 0.15 );
-  hemiLight.groundColor.setHSL( 0.0, 0.0, 0.7 );
-  hemiLight.position.y = 1500;
-  scene.add( hemiLight );
-
-  //var vertexShader = document.getElementById( 'vertexShader' ).textContent;
-  var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
-  var uniforms = {
-      topColor:    { type: "c", value: new THREE.Color( 0x0077ff ) },
-      bottomColor: { type: "c", value: new THREE.Color( 0xffffff ) },
-      offset:    { type: "f", value: 400 },
-      exponent:  { type: "f", value: 0.6 }
-  }
-  uniforms.topColor.value.copy( hemiLight.color );
-
-  // GROUND
-
-  var groundGeo = new THREE.PlaneBufferGeometry( 1000, 1000 );
-  var groundMat = new THREE.MeshPhongMaterial( { ambient: 0xffffff, color: 0xffffff, specular: 0x050505 } );
-  groundMat.color.setHSL( 0.095, 0.0, 0.75 );
-
-  var ground = new THREE.Mesh( groundGeo, groundMat );
-    ground.position.x = 500;
-
-  ground.rotation.x = -Math.PI/2;
-  ground.position.y = 0;
-
-  //group.add( ground );
 }
 
-function fetchData(l, lo) {
+function fetchData(lon, lat) {
   document.getElementById('loading-bar').style.display = "block";
   var buildings = [];
   var heights = [];
 
   scene.remove(buildingGroup);
 
-
-  var lat = parseFloat(l);//-74.0059700;
-  var lon = parseFloat(lo);//40.7142700;
+  var lon = parseFloat(lon);//-74.0059700;
+  var lat = parseFloat(lat);//40.7142700;
 
   var projection = d3.geo.mercator()
-    .center([lat, lon])
+    .center([lon, lat])
     .scale([6000000])
     .precision(.0);
 
@@ -133,39 +102,42 @@ function fetchData(l, lo) {
 
   // this is the part converting d3 path to three shape
 
-  var requestLat = long2tile(lat,  16);
-  var requestLon = lat2tile(lon , 16);
+  var requestLon = long2tile(lon,  16);
+  var requestLat = lat2tile(lat , 16);
   var key = "vector-tiles-xaDJOzg";
   var dataKind = "earth,water,buildings"
   var zoomLevel = "16";
 
-  var baseurl = "http://vector.mapzen.com/osm/"+dataKind+"/"+zoomLevel+"/"+requestLat + "/" + requestLon + ".json?api_key="+key;
+  var baseurl = "http://vector.mapzen.com/osm/"+dataKind+"/"+zoomLevel+"/"+requestLon + "/" + requestLat + ".json?api_key="+key;
 
-  d3.json(baseurl, function(json) {
-
-    for(obj in json){
-      for(j = 0; j< json[obj].features.length; j++) {
-        var geoFeature = json[obj].features[j];
-        var properties = geoFeature.properties;
-        var feature = path(geoFeature);
-        // 'a' command is not implemented in d3-three, skipiping for now.
-        if(feature.indexOf('a') > 0) console.log('wooh there is dangerous command here');
-        else {
-          var mesh = transformSVGPathExposed(feature);
-          buildings.push(mesh);
-          var h = geoFeature.properties["height"] || 1;
-          heights.push(h);
+  d3.json(baseurl, function(err,json) {
+    if(err) console.log('err!');
+    else {
+      console.log(json);
+      for(obj in json){
+        for(j = 0; j< json[obj].features.length; j++) {
+          var geoFeature = json[obj].features[j];
+          var properties = geoFeature.properties;
+          var feature = path(geoFeature);
+          // 'a' command is not implemented in d3-three, skipiping for now.
+          if(feature.indexOf('a') > 0) console.log('wooh there is dangerous command here');
+          else {
+            var mesh = transformSVGPathExposed(feature);
+            buildings.push(mesh);
+            var h = geoFeature.properties["height"] || 1;
+            heights.push(h);
+          }
         }
       }
+
+      var obj = {};
+      obj.paths = buildings;
+      obj.amounts = heights;
+
+      buildingGroup = new THREE.Group();
+      scene.add( buildingGroup );
+      addGeoObject( buildingGroup, obj );
     }
-
-    var obj = {};
-    obj.paths = buildings;
-    obj.amounts = heights;
-
-    buildingGroup = new THREE.Group();
-    scene.add( buildingGroup );
-    addGeoObject( buildingGroup, obj );
   });
 }
 
@@ -179,33 +151,30 @@ function onWindowResize() {
 
 
 
-  function animate() {
+function animate() {
+  requestAnimationFrame( animate );
+  controls.update();
+  renderer.render( scene, camera );
+};
 
-    requestAnimationFrame( animate );
-    controls.update();
-    renderer.render( scene, camera );
+/// Main
 
-  };
+var renderer;
+var scene, camera, group, buildingGroup;
 
-  /// Main
+var container = document.createElement( 'div' );
+document.body.appendChild( container );
 
-  var renderer;
-  var scene, camera, group, buildingGroup;
+init3d();
 
-  var container = document.createElement( 'div' );
-  document.body.appendChild( container );
+container.appendChild( renderer.domElement );
 
-  init3d();
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = false;
 
-  container.appendChild( renderer.domElement );
-
-  var controls = new THREE.OrbitControls( camera, renderer.domElement );
-  //controls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.25;
-  controls.enableZoom = false;
-
-  animate();
+animate();
 
 function exportToObj () {
   var exporter = new THREE.OBJExporter ();
@@ -218,12 +187,10 @@ window.onload = function() {
   var exportBtn = document.getElementById('exportBtn');
 
   exportBtn.addEventListener( 'click', function() {
-
-    var inputLat = document.getElementById('lat').value;
     var inputLon = document.getElementById('lon').value;
-    fetchData(inputLat, inputLon);
+    var inputLat = document.getElementById('lat').value;
+    fetchData(inputLon, inputLat);
   });
-
 
   window.addEventListener( 'resize', onWindowResize, false );
 }
@@ -232,6 +199,17 @@ window.onload = function() {
 function long2tile(lon,zoom) {
   return (Math.floor((lon+180)/360*Math.pow(2,zoom)));
 }
- function lat2tile(lat,zoom)  {
+function lat2tile(lat,zoom)  {
   return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom)));
 }
+
+//shold check it will work
+function tile2long(tileLon, zoom) {
+  return (tile*360/Math.pow(2,zoom));
+}
+
+function tile2lat(tileLat, zoom) {
+  return (360/Math.PI) * Math.atan(Math.pow( Math.E, (Math.PI - 2*Math.PI*tileLat/(Math.pow(2,zoom)))))-90
+}
+
+//L = (360 / PI) * atan(e^(PI - 2 * PI * Y / (2 ^ Z))) - 90
