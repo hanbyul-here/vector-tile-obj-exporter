b69ce712d7ede6d652f6acd505a042b9ce1a92bc
burritojustice <john@mapzen.com>
2016-09-12 23:30:49 -0700

    config.zoomLevel

--- js/tile-exporter.js
+++ js/tile-exporter.js
@@ -391,7 +391,7 @@ var TileExporter = (function() {
     var buildingObj = exportToObj()
     var exportA = document.getElementById('exportA');
     exportA.className = "";
-    exportA.download = 'tile-'+tileLon +'-'+tileLat+'-z'+zoom+'.obj';
+    exportA.download = 'tile-'+tileLon +'-'+tileLat+'-z'+config.zoomLevel+'.obj';
 
 
     var blob = new Blob([buildingObj], {type: 'text'});
